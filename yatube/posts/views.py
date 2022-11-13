7357c3a2239eb80003c87c046b460d26b399f1a7
Кирилл Черный <kirillcerny@gmail.com>
2022-11-13 03:52:14 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+def group_posts(request, slug):
+    return HttpResponse(f'Страница с постами группы: {slug}')
