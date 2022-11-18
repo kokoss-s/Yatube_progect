b50c9172874b9947f0194be48225f353db3fc560
Кирилл Черный <kirillcerny@gmail.com>
2022-11-18 17:20:48 +0300

    БД, миграции, адмика, тестовые данные и изменение шаблонов index & group_list.

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -12,3 +12,19 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
+    group = models.ForeignKey(
+        'Group',
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='posts',
+    )
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=30)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
